2dd87900c55f60b588bd6af48fb49ca3ea12ccc2
prasidha <acharyaprasidha08@gmail.com>
2024-05-16 16:06:13 +0545

    enchanced card and used extended tailwind

--- src/app/components/ProductCards.tsx
+++ src/app/components/ProductCards.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
+import Tooltip from "./Tooltip";
 
 export type Product = {
   id: string;
@@ -33,12 +34,13 @@ const mainContainer = {
     },
   },
   hover: {
+    outerWidth: "520px",
     x: 30,
     scale: 1.1,
     borderRaduis: "50%",
     transition: {
       backgroundColor: "red",
-      duration: 0.4,
+      duration: 2,
       type: "spring",
       damping: 10,
       stiffness: 400,
@@ -64,7 +66,6 @@ const deleteContainer = {
 function ProductCards({ product }: ProductCardsProps) {
   const { title, thumbnail, discountPercentage, description, price, category } =
     product;
-  const [isMouseEntered, setIsMouseEntered] = useState(false);
 
   const getTotalPriceAfterDiscount = (discount: number, totalPrice: number) => {
     const totalPriceAfterDiscount = totalPrice - (discount / 100) * totalPrice;
@@ -73,76 +74,65 @@ function ProductCards({ product }: ProductCardsProps) {
 
   return (
     <motion.div
-      className="rounded-lg relative"
+      className="flex flex-wrap mt-[1.9rem] w-[501px] h-[118px]"
       initial="rest"
       whileHover="hover"
       animate="rest"
     >
       {/* this is a card */}
       <motion.div
-        className="flex justify-between p-2  gap-[20rem] bg-white"
+        className="flex items-center p-2 bg-white w-full gap-4 px-[24px] py-[14px]"
         variants={mainContainer}
       >
-        <div className="flex gap-8">
-          <Tooltip text="this is product sdfsdfsjlfjorewjflksadflsjflsddkfjlsflsdfo">
-            <div className="self-center cursor-pointer ">
-              <Image
-                src={thumbnail}
-                alt="image"
-                width={110}
-                height={140}
-                className="rounded-lg"
-              />
+        <div>
+          <Tooltip title={title} description={description}>
+            <div className="self-center cursor-pointer min-h-[90px] min-w-[88px] ">
+              <Image src={thumbnail} alt="image" className="rounded-lg" fill />
             </div>
           </Tooltip>
-          <div className="flex flex-col max-w-[400px]">
-            <div className="flex gap-4">
-              <div>{title}</div>
-              <div>{category}</div>
-            </div>
-            <div>{description}</div>
-            <div>1st May, 2024</div>
-          </div>
         </div>
 
-        <div className="flex flex-col justify-between gap-2">
-          <div>{discountPercentage}%</div>
+        <div className="flex w-full">
+          {/* title decription */}
+          <div className="flex flex-col max-w-[247px] gap-2">
+            <div className="flex gap-4 items-center">
+              <div className="text-sm font-medium leading-snug">{title}</div>
+              <p className="bg-lightPink px-[14px] py-[4px] font-normal text-center text-xxs leading-firm rounded-md">
+                {category}
+              </p>
+            </div>
+            <p className="font-light text-xs leading-tight text-random">
+              {description}
+            </p>
+            <p className="font-medium leading-snug text-sm text-gray ">
+              1st May, 2024
+            </p>
+          </div>
+          {/* price  */}
+          <div className="font-bricolageGrotesque flex flex-col justify-between gap-2 flex-[150px] text-end w-[91px]">
+            <p className=" text-red font-medium text-sm leading-snug">
+              {discountPercentage}% OFF
+            </p>
 
-          <div>
-            <div className="line-through">${price}</div>
-            <div>${getTotalPriceAfterDiscount(discountPercentage, price)}</div>
+            <div>
+              <p className="line-through text-md font-normal leading-normal text-lightGray">
+                ${price}
+              </p>
+              <p className="font-bold text-lg leading-medium">
+                ${getTotalPriceAfterDiscount(discountPercentage, price)}
+              </p>
+            </div>
           </div>
         </div>
       </motion.div>
 
       {/* card ends here */}
 
-      <motion.div
-        variants={deleteContainer}
-        className="absolute opacity-0 h-[110.50px] bg-red-500 top-0 -right-[8rem] w-[100px] translate-x-[50%] "
-      >
+      {/* <motion.div variants={deleteContainer} className="absolute bg-[red] ">
         delete icon
-      </motion.div>
+      </motion.div> */}
     </motion.div>
   );
 }
 
 export default ProductCards;
-
-function Tooltip({ children, text }: any) {
-  return (
-    <span className="group relative">
-      <span
-        className="absolute -top-[3.2rem] -left-5 -translate-x-[70%] whitespace-nowrap bg-white 
-      p-2 opacity-0 transition group-hover:opacity-100  before:content[''] before:absolute before:top-full 
-      before:right-4 before:border-[1rem] before:border-transparent before:border-t-[white]"
-      >
-        <div>
-          <div>text product</div>
-          <div>this is description of image of product</div>
-        </div>
-      </span>
-      {children}
-    </span>
-  );
-}
