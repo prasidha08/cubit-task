ed44737d056d8361a62bc9c031156eb827b0fae3
prasidha <acharyaprasidha08@gmail.com>
2024-05-16 23:56:18 +0545

    given gap to each card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import ProductLists from "./components/ProductLists";
 async function fetchProducts() {
   const backendURL = config.backendURL;
 
-  const projects = await fetch(`${backendURL}/products?limit=2`, {
+  const projects = await fetch(`${backendURL}/products?limit=10`, {
     method: "get",
   });
   const response = await projects.json();
@@ -14,7 +14,7 @@ async function fetchProducts() {
 export default async function Home() {
   const productResponse = await fetchProducts();
   return (
-    <main className="relative flex flex-col items-center pt-[10rem]">
+    <main className="relative flex flex-col items-center pt-[10rem] pb-[10rem] h-[100vh] overflow-auto gap-10">
       <div className="text-white text-title font-medium mb-[10px]">
         🔥 Special Offers
       </div>
