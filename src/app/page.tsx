d94c4d0c232ee37ead33aff6d3deb38ab8440884
prasidha <acharyaprasidha08@gmail.com>
2024-05-16 16:06:41 +0545

    made a separate component for productlists

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
-"use client";
-
-import ProductCards from "./components/ProductCards";
-import { AnimatePresence } from "framer";
 import { config } from "./_config/config";
+import ProductLists from "./components/ProductLists";
 
 async function fetchProducts() {
   const backendURL = config.backendURL;
@@ -13,21 +10,16 @@ async function fetchProducts() {
   const response = await projects.json();
   return response.products;
 }
+
 export default async function Home() {
   const productResponse = await fetchProducts();
   return (
-    <main className="relative flex flex-col z-[999] items-center pt-[10rem]">
-      <div className="text-white">🔥 Special Offers</div>
-      {/* this is for card */}
-      <div className="mt-12 flex flex-col gap-10">
-        {/* <AnimatePresence initial={false}> */}
-          {productResponse?.map((product: any, index: any) => {
-            return (
-              <ProductCards product={product} key={`${index}_${product.id}`} />
-            );
-          })}
-        {/* </AnimatePresence> */}
+    <main className="relative flex flex-col items-center pt-[10rem]">
+      <div className="text-white text-title font-medium mb-[10px]">
+        🔥 Special Offers
       </div>
+      {/* this is for card */}
+      <ProductLists products={productResponse} />
     </main>
   );
 }
