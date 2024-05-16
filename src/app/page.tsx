775fc795ca0b08f5251219706baafa69c2e6b40f
prasidha <acharyaprasidha08@gmail.com>
2024-05-17 00:02:20 +0545

    put gap between icon and text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,8 +15,9 @@ export default async function Home() {
   const productResponse = await fetchProducts();
   return (
     <main className="relative flex flex-col items-center pt-[10rem] pb-[10rem] h-[100vh] overflow-auto gap-10">
-      <div className="text-white text-title font-medium mb-[10px]">
-        🔥 Special Offers
+      <div className="text-white text-title font-medium mb-[10px] flex items-center gap-4">
+        <span>🔥</span>
+        <p>Special Offers</p>
       </div>
       {/* this is for card */}
       <ProductLists products={productResponse} />
