e3f98e778951c184f67ff22bbb7d4c3e546a081d
prasidha <acharyaprasidha08@gmail.com>
2024-05-16 16:04:35 +0545

    extended tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,39 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      fontFamily: {},
+      colors: {
+        red: "#F41010",
+        lightGray: "#959595",
+        gray: "#7B7368",
+        white: "#FFFFFF",
+        lightPink: "#EDE1FF",
+        davyGray: "#535353",
+        black: "#000000",
+      },
+      fontFamily: {
+        bricolageGrotesque: "Bricolage Grotesque",
+      },
       fontWeight: {
-        "700": "700",
+        light: "400",
+        normal: "600",
+        medium: "700",
+        bold: "800",
       },
       fontSize: {
+        xxs: "10px",
+        xs: "12px",
+        sm: "14px",
+        md: "16px",
+        lg: "18px",
         title: "28px",
+      },
+      lineHeight: {
+        firm: "12px",
+        tight: "14.4px",
+        snug: "16.8px",
+        normal: "19.2px",
+        medium: "21.6px",
+        large: "33.6px",
       },
     },
   },
