65b7de6781afc3655621eabc3383a3144ed70b97
prasidha <acharyaprasidha08@gmail.com>
2024-05-17 22:07:07 +0545

    wraped bg colors with div and used svg to get gooey effect

--- src/app/components/Background.tsx
+++ src/app/components/Background.tsx
@@ -3,19 +3,10 @@ import React from "react";
 function Background() {
   return (
     <>
-      <div
-        className="bg-blue absolute h-[100vh] w-full"
-        // style={{
-        //   filter: "url(#goo) ",
-        // }}
-      >
-        {/* <div className="pink-circle top-[20rem] left-[60rem] animate-circle1"></div>
-        <div className="pink-circle top-[10rem] left-[40rem] animate-circle2"></div>
-        <div className="pink-circle bottom-[10rem] left-[30rem]"></div> */}
-
-        <svg xmlns="http://www.w3.org/2000/svg">
+      <div className=" bg-blue absolute h-[100vh] w-full">
+        <svg className="hidden">
           <defs>
-            <filter id="goo">
+            <filter id="blend">
               <feGaussianBlur
                 in="SourceGraphic"
                 stdDeviation="10"
@@ -25,12 +16,22 @@ function Background() {
                 in="blur"
                 mode="matrix"
                 values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
-                result="goo"
+                result="blend"
               />
-              <feBlend in="SourceGraphic" in2="goo" />
+              <feBlend in="SourceGraphic" in2="blend" />
             </filter>
           </defs>
         </svg>
+
+        <div style={{ filter: "url(#blend) blur(150px)" }}>
+          <div className="pink-circle animate-newCircleOne left-1/4 top-14 mix-blend-hard-light"></div>
+
+          <div className="pink-circle animate-newCircleTwo left-1/2 top-14"></div>
+
+          <div className="pink-circle animate-newCircleThree left-1/3 top-[20rem]"></div>
+
+          <div className="pink-circle w-[10rem] h-[10rem] -right-10 -top-10 animate-diagonalAnimation"></div>
+        </div>
       </div>
     </>
   );
